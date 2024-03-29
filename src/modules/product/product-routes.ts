7c065c08cf130f5e608b007ad93068d88a777b46
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-28 22:27:23 -0300

    feat: add route to find product by slug

--- src/modules/product/product-routes.ts
+++ src/modules/product/product-routes.ts
@@ -5,5 +5,6 @@ import ProductController from "./product-controller";
 const router: Router = express.Router();
 
 router.get("/", ProductController.list);
+router.get("/:slug", ProductController.findBySlug);
 
 export default router;
