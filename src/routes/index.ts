1ea9c96240e4c85b26744ef6a9188eb91a659202
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 13:46:36 -0300

    feat: add purchases routes to index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,13 @@ const router = Router();
 import { returnSuccessResponse } from "../util/http-response";
 
 import productRoutes from "../modules/product/product-routes";
+import purchasesRoutes from "../modules/purchase/purchase-routes";
 
 router.get("/", (_, response) =>
   returnSuccessResponse({ response, message: "OK!" })
 );
 
 router.use("/products", productRoutes);
+router.use("/purchases", purchasesRoutes);
 
 export default router;
